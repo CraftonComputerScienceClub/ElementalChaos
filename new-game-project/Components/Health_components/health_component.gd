c5e819e63c866a66535d154882856fe8f837fde7
D3RPSIDE <nicolas.melvill@gmail.com>
2024-10-22 18:11:26 -0700

    stuff

--- new-game-project/Components/Health_components/health_component.gd
+++ new-game-project/Components/Health_components/health_component.gd
@@ -1,4 +1,20 @@
 extends Node
 class_name Health
+signal Health_update
+signal Health_depleted
+@export var max_health:int
+var current_health:int
 
-@export var health_numb:int
+
+func _ready() -> void:
+	pass
+
+func set_current_healthmax():
+	current_health = max_health
+	pass
+	
+func damage(amount:int)->void:
+	current_health =- amount
+	
+func heal(amount:int)->void:
+	damage(-amount)
--- new-game-project/Game_Scenes/Test_scene/main_scene.gd
+++ new-game-project/Game_Scenes/Test_scene/main_scene.gd
@@ -3,7 +3,9 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	var current = 0
+	current =- 2
+	print(current)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
